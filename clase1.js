1b0b7ca9363c53c533216a6ffca5b5bffcffe303
wulf <czh1983@gmail.com>
2021-12-15 11:02:13 -0300

    Desafio nro 1 Javascript para CoderHouse

---
+++ clase1.js
@@ -0,0 +1,31 @@
+//Calculadora
+let dato1 = prompt("Ingrese un numero: ");
+let dato2 = prompt("Ingrese otro numero: ");
+let operacion = prompt("Elija una operacion a realizar: + , - , * , / ")
+
+
+let numero1 = parseInt(dato1);
+let numero2 = parseInt(dato2);
+
+let resultado = null;
+let texto = null;
+
+if (operacion === "+"){
+    //realizamos las operaciones matematicas
+    resultado = numero1 + numero2;
+    texto = "La suma de " + numero1 + " y " + numero2 + " es igual a " + resultado ;
+} else if (operacion === "-"){
+    resultado = numero1 - numero2;
+    texto = "La resta de " + numero1 + " y " + numero2 + " es igual a " + resultado ;
+} else if ( operacion === "*"){
+    resultado = numero1 * numero2;
+    texto = "La multiplicacion  de " + numero1 + " y " + numero2 + " es igual a " + resultado ;
+} else if(operacion === "/"){
+    resultado = numero1 / numero2;
+    texto = "La division de " + numero1 + " y " + numero2 + " es igual a " + resultado ;
+} else {
+    texto = "operacion no permitida"
+}
+
+alert(texto);
+console.log(texto);
