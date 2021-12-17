7e80092847a52034a6b4bf7cfd4c3207463a41d4
wulf <czh1983@gmail.com>
2021-12-16 22:56:00 -0300

    actualizado el desafio 2

--- Ejercicios/clase2.js
+++ Ejercicios/clase2.js
@@ -1,16 +1,16 @@
 //Alarma:
-let hora = prompt("Que hora es?(am/pm): ")
-let horario = parseInt(hora)
+let hora = prompt("Que hora es?(am/pm): ");
+let horario = parseInt(hora);
 
 
 
 if(horario >= 5  && horario <= 13){
-   alert("Buenos Dias")
+   alert("Buenos Dias");
 } else if(horario <= 20 && horario >= 14){
-    alert("Buenas Tardes")
+    alert("Buenas Tardes");
 } else if(horario >= 21 && horario <= 00){
-    alert("Buenas Noches")
+    alert("Buenas Noches");
 } else{
-    alert("Deberias estar durmiendo, ya no tenes 20!!")
+    alert("Deberias estar durmiendo, ya no tenes 20!!");
 }
 
